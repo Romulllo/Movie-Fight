b6ce348ff384df6c2d91d50a0031e4fc19c403d7
Romulo Santos <romulosantos88@hotmail.com>
2022-07-04 15:36:57 +0100

    fix msg error

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
         apikey: 'c18c7877',
-        s: 'avengers'
+        s: searchTerm
     }
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = event => {
-  fetchData(event.target.value);
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
+  }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
