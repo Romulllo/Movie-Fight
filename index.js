8be8ad0ce0cdb7305a4e9fbabac303c5e9d661c7
Romulo Santos <romulosantos88@hotmail.com>
2022-07-04 16:50:36 +0100

    dropdown

--- index.js
+++ index.js
@@ -13,20 +13,35 @@ const fetchData = async searchTerm => {
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
 
+  dropdown.classList.add('is-active');
   for (let movie of movies) {
-    const div = document.createElement('div');
+    const option = document.createElement('a');
 
-    div.innerHTML = `
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+      ${movie.Title}
     `;
 
-    document.querySelector('#target').appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
